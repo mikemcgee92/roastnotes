ee490124536c34d4cbbbe6e8ca9798c4a21f2809
Mike McGee <mmcgee738@gmail.com>
2025-02-24 18:08:51 -0600

    Set up dashboard page and READ on coffees

--- src/components/CoffeeCard.js
+++ src/components/CoffeeCard.js
@@ -1,16 +1,39 @@
 'use client';
 
-// import Image from 'next/image';
+import { Button } from 'react-bootstrap';
+// import { useRouter } from 'next/navigation';
 import PropTypes from 'prop-types';
 // import roastLevel from '../api/roastLevels';
 
 export default function CoffeeCard({ coffee }) {
-  return <div>{coffee.name}</div>;
+  // const router = useRouter();
+
+  return (
+    /* From Uiverse.io by Yaya12085 */
+    <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+      <div className="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+        <img src={coffee.image} alt={coffee.name} />
+      </div>
+      <div className="p-6">
+        <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">{coffee.name}</h5>
+        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">{coffee.origin}</p>
+        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">{coffee.tastingNotes.map((note) => `${note}, `)}</p>
+      </div>
+      <div className="p-6 pt-0">
+        <Button data-ripple-light="true" type="button" className="btn">
+          View
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 CoffeeCard.propTypes = {
   coffee: PropTypes.shape({
     id: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    origin: PropTypes.string,
+    tastingNotes: PropTypes.arrayOf,
   }).isRequired,
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,23 +6,27 @@ import { signOut } from '../utils/auth';
 
 export default function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-        <Link passHref href="/" className='navbar-brand'>
-         CHANGE ME
+        <Link passHref href="/" className="navbar-brand">
+          RoastNotes
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link className="nav-link" href="/">
-              Home
+              home
+            </Link>
+            <Link className="nav-link" href="/">
+              saved
+            </Link>
+            <Link className="nav-link" href="/">
+              +add
             </Link>
           </Nav>
 
-          <Button variant="danger" onClick={signOut}>
-              Sign Out
-          </Button>
+          <Button onClick={signOut}>Sign Out</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
